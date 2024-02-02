9271e132b50de300e4b31e43dc66d96bb2348663
ramiechaarani <ramie.chaarani@gmail.com>
2024-02-02 10:21:03 -0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,10 +8,13 @@ export default function Home() {
         <meta name="description" content="Coop Recs Frame" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="fc:frame" content="vNext" />
-        <meta name="fc:frame:image" content="/red.png" />
+        <meta
+          name="fc:frame:image"
+          content="https://mintnft-ruddy.vercel.app/red.png"
+        />
         <meta name="og:image" content="op.png" />
         <meta name="fc:frame:post_url" content="https://mintnft.vercel.app/" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="https://mintnft-ruddy.vercel.app/favicon.ico" />
       </Head>
       <div>This is a Farcaster frame. What are you doing here?</div>
     </>
