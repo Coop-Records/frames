101bca3b0328c8f1cc41a1da8beed64670195216
ramiechaarani <ramie.chaarani@gmail.com>
2024-02-06 16:22:10 -0800

    Update mint.ts

--- pages/api/mint.ts
+++ pages/api/mint.ts
@@ -59,14 +59,15 @@ export default async function handler(
       throw new Error();
     }
 
-    if (await isMintingSoldOut(address)) {
-      soldoutScreen(res);
-    } else if (await didUserAlreadyMint(address)) {
-      alreadyMintedScreen(res);
-    } else {
-      await mintSong(address);
-      successScreen(res);
-    }
+    // TODO: Disabled.
+    // if (await isMintingSoldOut(address)) {
+    //   soldoutScreen(res);
+    // } else if (await didUserAlreadyMint(address)) {
+    //   alreadyMintedScreen(res);
+    // } else {
+    //   await mintSong(address);
+    //   successScreen(res);
+    // }
   } catch (error) {
     console.error("Request failed:", error);
     res.status(500).json({ error: "Internal Server Error" });
