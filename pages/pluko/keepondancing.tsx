db73a7dd89d29f6f8376713dab5d73897a8ccc78
ramiechaarani <ramie.chaarani@gmail.com>
2024-02-08 16:43:47 -0800

    add pluko support

---
+++ pages/pluko/keepondancing.tsx
@@ -0,0 +1,28 @@
+import Head from "next/head";
+
+export default function Home() {
+  const endpointLocal =
+    "https://e623-2601-645-8a00-9db0-b5aa-f69a-f69-7ac2.ngrok-free.app";
+  const endpointProd = "https://frames.cooprecords.xyz";
+  return (
+    <>
+      <Head>
+        <title>Coop Recs Frame</title>
+        <meta name="description" content="Coop Recs Frame" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="fc:frame" content="vNext" />
+        <meta
+          name="fc:frame:image"
+          content={`${endpointProd}/plukokeepondancing/mint.png`}
+        />
+        <meta name="og:image" content="mint.png" />
+        <meta
+          name="fc:frame:post_url"
+          content={`${endpointProd}/api/pluko/keepondancing`}
+        />
+        <meta name="fc:frame:button:1" content="Collect" />
+      </Head>
+      <div>This is a Farcaster frame. What are you doing here?</div>
+    </>
+  );
+}
