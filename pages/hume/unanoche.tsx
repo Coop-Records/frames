5f207ff6820c0f8d7771f9660a432dd83e6806ba
ramiechaarani <ramie.chaarani@gmail.com>
2024-02-21 14:55:24 -0800

    add unanoche

--- pages/hume/unanoche.tsx
+++ pages/hume/unanoche.tsx
@@ -1,9 +1,8 @@
 import Head from "next/head";
 
 export default function Home() {
-  const endpointLocal =
-    "https://d964-2601-645-8a00-9db0-510b-8774-e7cd-76bd.ngrok-free.app";
-  const endpointProd = "https://frames.cooprecords.xyz";
+  const endpointLocal = "https://b456-104-59-124-88.ngrok-free.app";
+  const endpointProd = endpointLocal; // = "https://frames.cooprecords.xyz";
   return (
     <>
       <Head>
@@ -13,14 +12,14 @@ export default function Home() {
         <meta name="fc:frame" content="vNext" />
         <meta
           name="fc:frame:image"
-          content={`${endpointProd}/toolsfmlbrisk/mint.png`}
+          content={`${endpointProd}/unanoche/mint.jpg`}
         />
-        <meta name="og:image" content="mint.png" />
+        <meta name="og:image" content="mint.jpg" />
         <meta
           name="fc:frame:post_url"
-          content={`${endpointProd}/api/toolsfml/brisk`}
+          content={`${endpointProd}/api/hume/unanoche`}
         />
-        {/* <meta name="fc:frame:button:1" content="Collect" /> */}
+        <meta name="fc:frame:button:1" content="Collect" />
       </Head>
       <div>This is a Farcaster frame. What are you doing here?</div>
     </>
